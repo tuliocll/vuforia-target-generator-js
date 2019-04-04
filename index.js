5f3dc5f9d963e3a4f9f16403223f324a6efbdbc6
tuliocll <r3gbip8a>
2019-04-04 14:52:08 -0300

    Add comments, make some code corrections and cleanup

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-console.log("inicializando gerador...")
+console.log("inicializando gerador...");
 
-const { createCanvas, Image } = require('canvas')
-const canvas = createCanvas(450, 600)
-const ctx = canvas.getContext('2d')
+const { createCanvas, Image } = require('canvas');
+const canvas = createCanvas(450, 600);
+const ctx = canvas.getContext('2d');
 const fs = require('fs');
 
-const imgs_titulos = [
-    "resources/imgs_titulo/1.jpg",
-    "resources/imgs_titulo/2.jpg",
-    "resources/imgs_titulo/3.jpg",
-    ]
+    var index = 0;
+    //The name prefix on the file, like "target_A.jpg"
+    var targetNamePrefix = "target_";
+    //The name o folder or path to save all targets, dont forget the /
+    var pathToSave = "/genereted/";
+
+    const imgs_titulos = [
+        "resources/imgs_titulo/1.jpg",
+        "resources/imgs_titulo/2.jpg",
+        "resources/imgs_titulo/3.jpg",
+        ];
     
     const imgs_fundos = [
     "resources/imgs_fundo/1.jpg",
@@ -34,27 +40,30 @@ const imgs_titulos = [
     "resources/imgs_fundo/20.jpg",
     ];
 
+    //Configure the size of "pixels" that compose the background
     const tamanhoPixel = {
         altura: 200,
         largura: 150,
         quebraLinha: 3,
         quantidade: 9,
     }
-    
+    //Array with image elements
     var objsImagensfundo = preLoad(imgs_fundos);
     var objsImagensTitulo =  preLoad(imgs_titulos);
-    
+    //The text that will be show on the target,
+    //Here i use only letter, but you can configure the function
+    //drawTexte() to words.
     var letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L"];
 
     function gerarNovo(letra){
         var ii = 0;
         var random = 0;
     
-        //desenhar o fundo
+        //drawing the background
         for(var i =0; i< tamanhoPixel.quantidade; i++){
-            
+            //get a random index
             random = Math.floor((Math.random() * imgs_fundos.length));
-    
+            //mkaing the lines and columns
             if(i<tamanhoPixel.quebraLinha){
                 ctx.drawImage(objsImagensfundo[random], (tamanhoPixel.largura*i), 0, tamanhoPixel.largura, tamanhoPixel.altura);
             }else{
@@ -70,29 +79,30 @@ const imgs_titulos = [
                 }
             }
         }
-
-    
-        //desenhar o titulo
+        //draw the title, geting a random index too
         random = Math.floor((Math.random() * imgs_titulos.length));
         console.log(random);
         ctx.drawImage(objsImagensTitulo[random], 0, (tamanhoPixel.altura)*2.7, (tamanhoPixel.largura)*3, 65);
-    
-        //desenhar sombra letra
-        ctx.font = "420px Arial";
-        ctx.fillStyle = "black";
-        ctx.fillText(letra, (tamanhoPixel.largura-50), tamanhoPixel.altura*2.3);
-        //desenha letra
-        ctx.font = "420px Arial";
-        ctx.fillStyle = "white";
-        ctx.fillText(letra, (tamanhoPixel.largura-40), tamanhoPixel.altura*2.2);
+        drawText(letra);
     }
     
+    //Function to draw the text on the midle of target
+    //Actuly configured to letter size a position
+    function drawText(text){
+        //Drawing the text shadow
+        ctx.font = "420px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText(text, (tamanhoPixel.largura-50), tamanhoPixel.altura*2.3);
+        //Drawing the text
+        ctx.font = "420px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText(text, (tamanhoPixel.largura-40), tamanhoPixel.altura*2.2);
+    }
     
-    
-    
+    //Turning all the images path on 
+    //Images object, its work like a "preload"
     function preLoad(object){
         var img = new Array();
-    
         for(var i=0; i < object.length; i++){
             img[i] = new Image();
             img[i].src = object[i];
@@ -100,19 +110,21 @@ const imgs_titulos = [
         return img;
     }
 
-var index = 0;
-function gerarLetras(letra){
-    gerarNovo(letra);
-const out = fs.createWriteStream(__dirname + '/test'+letra+'.jpg')
-const stream = canvas.createJPEGStream()
-stream.pipe(out)
-out.on('finish', () => {
-    console.log('Letras '+letras[index]+' gerada com sucesso!')
-    index++;
-    if(index<letras.length){
-        gerarLetras(letras[index]);
+    
+    function gerarLetras(letra){
+        gerarNovo(letra);
+        const out = fs.createWriteStream(__dirname + pathToSave + targetNamePrefix + letra +'.jpg')
+        const stream = canvas.createJPEGStream()
+        stream.pipe(out)
+        out.on('finish', () => {
+            console.log('Target '+letras[index]+' generated!')
+            index++;
+            //if have more text in the array execute again
+            if(index<letras.length){
+                gerarLetras(letras[index]);
+            }
+        })
     }
-})
-}
 
+//Initialize the program
 gerarLetras(letras[index])
